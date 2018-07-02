d07c18009e2bef1df6bf9064cf4a42770c597290
StephaneC <Stephane.castrec@gmail.com>
2018-07-02 16:03:57 +0200

    step 6 ok

---
+++ step_6/src/pages/messages/message.ts
@@ -0,0 +1,5 @@
+export class Message {
+    timestamp: number;
+    username: string;
+    message: string;
+  }
---
+++ step_6/src/pages/tabs/tabs.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavParams} from 'ionic-angular';
+
+import { MessagesPage } from '../messages/messages';
+import { ContactPage } from '../contact/contact';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = MessagesPage;
+  tab2Root = ContactPage;
+
+  constructor(public navParams: NavParams) {
+    sessionStorage.setItem("token", this.navParams.get('token'));
+
+  }
+}
